3465f29800d78bcb0b30ddfd0fe29dcbaf701b34
Martin Hallén <marthall@outlook.com>
2013-12-18 00:17:38 +0100

    Added argument support

--- pycloc.py
+++ pycloc.py
@@ -1,11 +1,18 @@
+#!/usr/bin/python
+# -*- coding: latin-1 -*-
+
 from __future__ import print_function
-import os, sys
+import os
+from argparse import ArgumentParser
 
 here = os.path.dirname(__file__)
 
 LANGUAGES = {
-    "Python": {"EXT": ["py"], "INLINE_COMMENT": ["#"], "BLOCK_COMMENT": ['"""', "'''"]}
+    "Python": {"EXT": ["py"],
+               "INLINE_COMMENT": ["#"],
+               "BLOCK_COMMENT": ['"""', "'''"]}
 }
+
 
 def line_count(file_name):
     number_of_lines = 0
@@ -26,14 +33,33 @@ def line_count(file_name):
         else:
             number_code_lines += 1
         number_of_lines += 1
-    
+
     print(80 * "-")
-    print("Language".ljust(20) + "files".rjust(10) + "blank".rjust(16) + "comment".rjust(16) + "code".rjust(16))
+    print("Language".ljust(20) + "files".rjust(10) + "blank".rjust(16) +
+          "comment".rjust(16) + "code".rjust(16))
     print(80 * "-")
     for i in range(1):
-        print(language.ljust(20) + "%10d%16d%16d%16d" % (100, number_of_blank_lines, number_of_comment_lines, number_code_lines))
+        print(language.ljust(20) + "%10d%16d%16d%16d" % (100,
+              number_of_blank_lines, number_of_comment_lines,
+              number_code_lines))
     print(80 * "-")
 
-    return number_of_lines, number_code_lines, number_of_comment_lines, number_of_blank_lines
+    return number_of_lines, number_code_lines, number_of_comment_lines,
+    number_of_blank_lines
 
-line_count(os.path.join(here, "test.py"))
+
+def get_args():
+    parser = ArgumentParser(description="Python line counting tool?",
+                            prog='pycloc')
+    parser.add_argument('-f', '--file', action='store_true', default=False,
+                        dest='ascii', help='input file')
+    return parser.parse_args()
+
+
+def main():
+    args = get_args()
+    line_count(os.path.join(here, "test.py"))
+
+
+if __name__ == '__main__':
+    main()
